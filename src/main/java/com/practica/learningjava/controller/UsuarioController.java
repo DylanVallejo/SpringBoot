b302ba9e0389ce6097fd6a332153fa62491d5a68
DylanVallejo <dylan1811vallejo@gmail.com>
2022-11-17 23:12:36 -0500

    implementing different https methods and endpoints to fetch data whit js

--- src/main/java/com/practica/learningjava/controller/UsuarioController.java
+++ src/main/java/com/practica/learningjava/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.practica.learningjava.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //import java.util.List;
 
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,16 +40,52 @@ public class UsuarioController {
 			
 		}
 	
-//	@RequestMapping(value = "prueba") //mapeo de la ruta con el servicio 
-//	public Usuario getUsuario() {
-//		Usuario usuario = new Usuario(); //new usuario from class Usuario
-//		usuario.setNombre("Dylan");
-//		usuario.setApellido("Vallejo");
-//		usuario.setEmail("dylan@gmail.com");
-//		usuario.setTelefono("1221212");
-//		return usuario;
-//		
-//	}
+	@RequestMapping(value = "usuarios") //mapeo de la ruta con el servicio 
+	public List<Usuario> getUsuarios() {
+		
+		List<Usuario> usuarios = new ArrayList<>();
+		Usuario usuario = new Usuario(); //new usuario from class Usuario
+		usuario.setId(12L);
+		usuario.setNombre("Dylan");
+		usuario.setApellido("Vallejo");
+		usuario.setEmail("dylan@gmail.com");
+		usuario.setTelefono("12324");
+		
+		Usuario usuario2 = new Usuario(); //new usuario from class Usuario
+		usuario2.setId(1234L);
+		usuario2.setNombre("qwerq");
+		usuario2.setApellido("Vallejo");
+		usuario2.setEmail("dylan@gmail.com");
+		usuario2.setTelefono("12341234");
+		
+		Usuario usuario3 = new Usuario(); //new usuario from class Usuario
+		usuario3.setId(12341234L);
+		usuario3.setNombre("qwerqwer");
+		usuario3.setApellido("Vallejo");
+		usuario3.setEmail("dylan@gmail.com");
+		usuario3.setTelefono("444123412");
+		
+
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		usuarios.add(usuario3);
+		
+		
+
+		return usuarios;
+		
+	}
+	@RequestMapping(value = "usuario123") //mapeo de la ruta con el servicio 
+	public Usuario editar() {
+		Usuario usuario = new Usuario(); //new usuario from class Usuario
+		usuario.setNombre("Dylan");
+		usuario.setApellido("Vallejo");
+		usuario.setEmail("dylan@gmail.com");
+		usuario.setTelefono("1221212");
+
+		return usuario;
+		
+	}
 //	@RequestMapping(value = "prueba") //mapeo de la ruta con el servicio 
 //	public Usuario getUsuario() {
 //		Usuario usuario = new Usuario(); //new usuario from class Usuario
